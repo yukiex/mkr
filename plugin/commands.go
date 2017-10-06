dfceccbcc26782c9a33e65a700bc87f0e9111b45
shibayu36 <shibayu36@gmail.com>
2017-10-06 17:01:50 +0900

    Implements parseInstallTarget

--- plugin/commands.go
+++ plugin/commands.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gopkg.in/urfave/cli.v1"
@@ -31,10 +32,16 @@ var CommandPlugin = cli.Command{
 }
 
 func doPluginInstall(c *cli.Context) error {
+	argInstallTarget := c.Args().First()
+	if argInstallTarget == "" {
+		return fmt.Errorf("Specify install name")
+	}
+
 	err := setupPluginDir(c.String("prefix"))
 	if err != nil {
 		return errors.Wrap(err, "failed to install plugin")
 	}
+
 	fmt.Println("do plugin install [wip]")
 	return nil
 }
@@ -48,4 +55,40 @@ func setupPluginDir(prefix string) error {
 		return errors.Wrap(err, "failed to setup plugin directory")
 	}
 	return nil
+}
+
+type installTarget struct {
+	owner      string
+	repo       string
+	pluginName string
+	releaseTag string
+}
+
+func parseInstallTarget(target string) (*installTarget, error) {
+	it := &installTarget{}
+
+	ownerRepoAndReleaseTag := strings.Split(target, "@")
+	var ownerRepo string
+	switch len(ownerRepoAndReleaseTag) {
+	case 1:
+		ownerRepo = ownerRepoAndReleaseTag[0]
+	case 2:
+		ownerRepo = ownerRepoAndReleaseTag[0]
+		it.releaseTag = ownerRepoAndReleaseTag[1]
+	default:
+		return nil, fmt.Errorf("Install target is invalid: %s", target)
+	}
+
+	ownerAndRepo := strings.Split(ownerRepo, "/")
+	switch len(ownerAndRepo) {
+	case 1:
+		it.pluginName = ownerAndRepo[0]
+	case 2:
+		it.owner = ownerAndRepo[0]
+		it.repo = ownerAndRepo[1]
+	default:
+		return nil, fmt.Errorf("Install target is invalid: %s", target)
+	}
+
+	return it, nil
 }
--- plugin/commands_test.go
+++ plugin/commands_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -39,6 +40,90 @@ func TestSetupPluginDir(t *testing.T) {
 		err = setupPluginDir(tmpd)
 		if err == nil {
 			t.Errorf("error should be occured while manipulate unpermitted directory, but it is nil")
+		}
+	}
+}
+
+func TestParseInstallTarget(t *testing.T) {
+	testCases := []struct {
+		Name   string
+		Input  string
+		Output installTarget
+	}{
+		{
+			Name:  "Plugin name only",
+			Input: "mackerel-plugin-sample",
+			Output: installTarget{
+				pluginName: "mackerel-plugin-sample",
+			},
+		},
+		{
+			Name:  "Plugin name and release tag",
+			Input: "mackerel-plugin-sample@v0.0.1",
+			Output: installTarget{
+				pluginName: "mackerel-plugin-sample",
+				releaseTag: "v0.0.1",
+			},
+		},
+		{
+			Name:  "Owner and repo",
+			Input: "mackerelio/mackerel-plugin-sample",
+			Output: installTarget{
+				owner: "mackerelio",
+				repo:  "mackerel-plugin-sample",
+			},
+		},
+		{
+			Name:  "Owner and repo with release tag",
+			Input: "mackerelio/mackerel-plugin-sample@v1.0.1",
+			Output: installTarget{
+				owner:      "mackerelio",
+				repo:       "mackerel-plugin-sample",
+				releaseTag: "v1.0.1",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Logf("testing: %s\n", tc.Name)
+		it, err := parseInstallTarget(tc.Input)
+		if err != nil {
+			t.Errorf("%s(err): error should be nil but: %+v", tc.Name, err)
+			continue
+		}
+		if !reflect.DeepEqual(*it, tc.Output) {
+			t.Errorf("%s(parse): \n out =%+v\n want %+v", tc.Name, *it, tc.Output)
+		}
+	}
+}
+
+func TestParseInstallTarget_error(t *testing.T) {
+	testCases := []struct {
+		Name   string
+		Input  string
+		Output string
+	}{
+		{
+			Name:   "Too many @",
+			Input:  "mackerel-plugin-sample@v0.0.1@v0.1.0",
+			Output: "Install target is invalid: mackerel-plugin-sample@v0.0.1@v0.1.0",
+		},
+		{
+			Name:   "Too many /",
+			Input:  "mackerelio/hatena/mackerel-plugin-sample",
+			Output: "Install target is invalid: mackerelio/hatena/mackerel-plugin-sample",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Logf("testing: %s\n", tc.Name)
+		_, err := parseInstallTarget(tc.Input)
+		if err == nil {
+			t.Errorf("%s(err): err should be occurred but nil", tc.Name)
+			continue
+		}
+		if err.Error() != tc.Output {
+			t.Errorf("%s(parse_error): \n out =%s\n want %s", tc.Name, err.Error(), tc.Output)
 		}
 	}
 }
